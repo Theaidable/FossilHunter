9e51ecb090ad0585e6a6d9fba0cf1ac085aba7fc
Hargaflarb <emmabrocher@gmail.com>
2025-11-11 11:13:19 +0100

    added code comments and clarified some names

--- Fossil Hunter/Assets/Core/Scripts/Click raycast.cs
+++ Fossil Hunter/Assets/Core/Scripts/Click raycast.cs
@@ -8,24 +8,27 @@ public class Clickraycast : MonoBehaviour
     LayerMask layerMask;
     [SerializeField]
     [Tooltip("The layer(s) that the click should not interact with")]
-    string[] layerHits;
+    string[] layerMasks;
     Camera mainCamera;
 
     void Awake()
     {
-        layerMask = ~LayerMask.GetMask(layerHits);
+        layerMask = ~LayerMask.GetMask(layerMasks);
         mainCamera = Camera.main;
     }
 
     void Update()
     {
+        //only check for raycast collision when the left mouse button is clicked
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(mainCamera.ScreenToWorldPoint(Input.mousePosition),Vector2.zero,0,layerMask);
+            //if the raycast hit anything in the non-masked layers
             if (hit.collider!=null)
             {
                 Debug.Log($"hit! {hit.collider.gameObject.name}");
             }
+            //if it didn't
             else
             {
                 Debug.Log("did not hit");
