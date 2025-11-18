77bdebaa296abeadbd6d3a0793341fa0a40d6393
Hvemvilhvad <malthewbirch@gmail.com>
2025-11-18 14:44:16 +0100

    can save data between scenes now

--- Fossil Hunter/Assets/Core/Scripts/StartMuseumManager.cs
+++ Fossil Hunter/Assets/Core/Scripts/StartMuseumManager.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class StartMuseumManager : MonoBehaviour
 {
+    [SerializeField] private GameObject museumItemPrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        MuseumItemManager.InitializeManager();
+        MuseumItemManager.InitializeManager(museumItemPrefab);
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+
 
 }
