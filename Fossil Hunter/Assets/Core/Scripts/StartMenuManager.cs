fcbc7521edbbfe70e4c9859f2b4026305161fdef
Eleavate <echoovervad@gmail.com>
2025-11-30 18:23:42 +0100

    Startscene stuff

---
+++ Fossil Hunter/Assets/Core/Scripts/StartMenuManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class StartMenuManager : MonoBehaviour
+{
+    private Button sandBtn;
+    private Button rockBtn;
+    private Button museumBtn;
+    private Button cleanerBtn;
+
+    private void OnEnable()
+    {
+        var doc = GetComponent<UIDocument>();
+        var root = doc.rootVisualElement;
+
+        sandBtn = root.Q<Button>("SandBeach");
+        sandBtn.clicked += OnSandBeachPressed;
+
+        rockBtn = root.Q<Button>("RockBeach");
+        rockBtn.clicked += OnStoneBeachPressed;
+
+        museumBtn = root.Q<Button>("Museum");
+        museumBtn.clicked += OnMuseumPressed;
+
+        cleanerBtn = root.Q<Button>("Cleaning");
+        cleanerBtn.clicked += OnCleaningPressed;
+    }
+    private void OnDisable()
+    {
+        sandBtn.clicked -= OnSandBeachPressed;
+        rockBtn.clicked -= OnStoneBeachPressed;
+        museumBtn.clicked -= OnMuseumPressed;
+        cleanerBtn.clicked -= OnCleaningPressed;
+    }
+    
+    private void OnSandBeachPressed()
+    {
+        Debug.Log("Pls");
+        SceneManager.LoadSceneAsync("Digging level 1", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("StartScreen");
+        MenuManager.currentScene = 4;
+        Debug.Log(MenuManager.currentScene);
+    }
+
+    private void OnStoneBeachPressed()
+    {
+        SceneManager.LoadSceneAsync("Digging level 2", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("StartScreen");
+        MenuManager.currentScene = 5;
+    }
+
+    private void OnMuseumPressed()
+    {
+        SceneManager.LoadSceneAsync("InfoPopUpScene", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("StartScreen");
+        MenuManager.currentScene = 3;
+    }
+
+    private void OnCleaningPressed()
+    {
+        SceneManager.LoadSceneAsync("Cleaning level", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("StartScreen");
+        MenuManager.currentScene = 2;
+    }
+}
